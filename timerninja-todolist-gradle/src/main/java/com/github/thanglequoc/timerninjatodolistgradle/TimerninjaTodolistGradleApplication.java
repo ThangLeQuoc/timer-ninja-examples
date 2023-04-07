b5c1446eb0dd7937b15cbc846433bfd93658c321
tle <tle@tripadvisor.com>
2023-04-07 11:01:40 +0700

    Add gradle project

---
+++ timerninja-todolist-gradle/src/main/java/com/github/thanglequoc/timerninjatodolistgradle/TimerninjaTodolistGradleApplication.java
@@ -0,0 +1,13 @@
+package com.github.thanglequoc.timerninjatodolistgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerninjaTodolistGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimerninjaTodolistGradleApplication.class, args);
+	}
+
+}
---
+++ timerninja-todolist-gradle/src/test/java/com/github/thanglequoc/timerninjatodolistgradle/TimerninjaTodolistGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thanglequoc.timerninjatodolistgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerninjaTodolistGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
